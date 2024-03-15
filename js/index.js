2594da4039da2b5708b055e91e2ff8035e6a2579
Daur <ndauir02@gmail.com>
2024-03-15 19:58:06 +0600

    Сделал 3 функционал игру

--- js/index.js
+++ js/index.js
@@ -24,84 +24,34 @@ const Hero = {
   showInfo: showInfo,
   modifyHealth: modifyHealth,
   attack: attack,
-  equipShield (){
-    if(!this.isShieldEquipped){
-      this.isShieldEquipped = true;
-      this.defense += this.shield;
-      console.log(this.defense);
-    }else{
-      console.log(`Герой уже носит щит! ${this.defense}`);
-    }
-  },
-  removeShield (){
-    if(this.isShieldEquipped){
-      this.isShieldEquipped = false;
-      this.defense -= this.shield;
-      console.log(this.defense);
-    }
-    else{
-      console.log(`не может снять щит, потому что он не экипирован! ${this.defense}`);
-    }
-  }
+  equipShield: equipShield,
+  removeShield:removeShield,
+}
 
-};
-
+const action = confirm('Добро пожаловать! Чтобы играть нажмите OK ');
 while(true){
-
-  Hero.attack();// Герой атакует
+  if(action === null){
+    break;
+  }else{
+    actionSwitch();
+    if(Dragon.hp <= 0){
+      break;
+    }
+    Dragon.attackDragon();// Дракон атакует
+    if(Hero.hp <= 0){
+      
+      console.log('Дракон победил');
+      break;
+      
+    }
   
-  if(Dragon.hp <= 0){ //Если удракона будет жизнь меньше или равно нулью 
-    
-    console.log('Герой победил');//Герой победил
-    break;
-    
-  }
-  Dragon.attackDragon();// Дракон атакует
-  if(Hero.hp <= 0){
-    
-    console.log('Дракон победил');
-    break;
-    
   }
   
 }
 
 
 
-// Уровень 3
-// Добавить возможность выбирать, что герой будет делать:
-// ● Атаковать (в этом случае производятся все действия, как и в 1-м уровне задания)
-// ● Ничего не делать
-// ● Защищаться
-// ● Щит (shield) героя прибавляется к значению защиты (defense) героя
-// ● Если выбрано действие “защищаться”, то увеличенное значение защиты
-// действует только на текущий ход (до следующего действия героя).
-// Указания:
-// ● Добавить герою методы equipShield и removeShield, которые будут добавлять и
-// отнимать у него количество щита к общей защите. При этом нужно произвести
-// проверку, что щит был экипирован раньше, чтобы не добавлять двойную порцию
-// щита, или не убавлять защиту меньше, чем базовая защита. Для этого можно
-// ввести булевое свойство isShieldEquipped, которое должно ставиться и убираться
-// в этом методе при успешной экипировке и успешном снятии. При попытке снять не
-// одетый щит или одеть щит, который уже был одет, выводить в консоль
-// соответствующие сообщения.
 
-
-function performAction(hero, action) {
-  switch (action) {
-      case 'attack':
-          return hero + ' атакует врага!';
-      case 'defend':
-          return hero + ' защищается от атаки врага.';
-      case 'heal':
-          return hero + ' лечит свои раны.';
-      default:
-          return 'Неизвестное действие для героя ' + hero + '.';
-  }
-}
-
-// Пример использования:
-console.log(performAction('Рыцарь', 'attack')); // Рыцарь атакует врага!
 
 
 
--- js/utils.js
+++ js/utils.js
@@ -60,4 +60,54 @@ const attackDragon = function() {
 
   }
 
+
+}
+
+const equipShield = function(){
+  if(!this.isShieldEquipped){
+    this.isShieldEquipped = true;
+    this.defense += this.shield;
+    console.log(this.defense);
+  }else{
+    console.log(`Герой уже носит щит! Текущая защита - ${this.defense} \n=====================`);
+  }
+}
+const removeShield = function(){
+  if(this.isShieldEquipped){
+    this.isShieldEquipped = false;
+    this.defense -= this.shield;
+    console.log(this.defense);
+  }
+  else{
+    console.log(`Не может снять щит, потому что он не экипирован! Текущая защита - ${this.defense} \n=====================`);
+  }
+}
+const actionSwitch = function (){
+  const action = prompt('Напишите одно из цифр чтобы выполнить одно из действие: 1)будет атаковать 2)Ниче не делать 3)защищаться');
+  switch (action) {
+    case '1':
+      Hero.removeShield();
+      Hero.attack();// Герой атакует
+
+      if(Dragon.hp <= 0){ //Если у дракона будет жизнь меньше или равно нулью 
+        
+        console.log('Герой победил');//Герой победил
+        break;
+        
+      }
+      break;
+    
+    case '2':
+      alert('Герой пропустил ход')
+      break;
+    case '3':
+      Hero.equipShield();
+      break;
+    case '':
+      break;
+    default:
+      alert('Введите цифр')
+      break;
+        
+  }
 }
